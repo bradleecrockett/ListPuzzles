9a727b370f0a3ce135f1a945bb7a7688e13cd652
bradleecrockett <19703290+bradleecrockett@users.noreply.github.com>
2017-10-31 12:09:25 -0700

    Add files via upload

---
+++ ListPuzzles.py
@@ -0,0 +1,67 @@
+# Name: First Last
+# Date:
+# Period:
+# Lab: ListPuzzles.py
+# Description:  List puzzles are a place to show your understanding of list methods/functions
+#       and traversing algorithms.  Complete the following functions as described.  Actual
+#       tests may be more extensive than the basic test shown.
+#
+#     Style - Code format, whitespace and PEP-8 style is followed making code easy to read.
+#     Comments - Blocks of code are well commented, every function has a descriptive comment.
+#     Tests -   The program runs as described in the specifications without errors(passes all tests).
+#
+
+# Return the number of even ints in the given array (should return an int).
+# Note: the % "mod" operator computes the remainder, e.g. 5 % 2 is 1.
+'''
+Sample Tests
+count_evens([2, 1, 2, 3, 4]) → 3
+count_evens([2, 2, 0]) → 3
+count_evens([1, 3, 5]) → 0
+'''
+def count_evens(list):
+    # +++ your code here +++
+
+    return "zero"
+
+
+# Given an array length 1 or more of ints, return the difference between
+# the largest and smallest values in the array (should return an int).
+# Note: the built-in min(v1, v2) and max(v1, v2) functions return the
+# smaller or larger of two values.
+'''
+Sample tests
+big_diff([10, 3, 5, 6]) → 7
+big_diff([7, 2, 10, 9]) → 8
+big_diff([2, 10, 7, 2]) → 8
+'''
+def big_diff(list):
+    # +++ your code here +++
+
+    return "zero"
+
+
+
+
+# Return the "centered" average of an array of ints, which we'll say is
+# the mean average of the values, except ignoring the largest and smallest
+# values in the array. If there are multiple copies of the smallest value,
+# ignore just one copy, and likewise for the largest value. Use int division
+# to produce the final average. You may assume that the array is length 3 or more.
+# (should return a float)
+'''
+Sample tests
+centered_average([1, 2, 3, 4, 100]) → 3
+centered_average([1, 1, 5, 5, 10, 8, 7]) → 5
+centered_average([-10, -4, -2, -4, -2, 0]) → -3
+'''
+def centered_average():
+    # +++ your code here +++
+
+    return 0.0
+
+
+def main():
+    #Sample tests here
+
+main()
